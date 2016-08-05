fc0fd57a456ebb2e4be40d65c56f76e8d00c2ad7
grisson <grisson@gmail.com>
2016-08-04 23:55:03 -0700

    add 4 channel radio data model

--- Transmitter/SerialLib/BaseDataPackage.cs
+++ Transmitter/SerialLib/BaseDataPackage.cs
@@ -8,5 +8,14 @@ namespace SerialLib
 {
     public class BaseDataPackage
     {
+        protected bool CheckValueRangeIncludeEdge(byte currentValue, byte min, byte max)
+        {
+            if ((currentValue < min) || (currentValue > max))
+            {
+                throw new ArgumentOutOfRangeException($"current value is {currentValue}. The range is ");
+            }
+
+            return true;
+        }
     }
 }
---
+++ Transmitter/SerialLib/RadioFourChannels.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerialLib
+{
+    class RadioFourChannels : BaseDataPackage
+    {
+        private byte throttle;
+        private byte yaw;
+        private byte pitch;
+        private byte roll;
+
+        public byte Throttle
+        {
+            get
+            {
+                return throttle;
+            }
+
+            set
+            {
+                if (CheckValueRangeIncludeEdge(value, 0, 255))
+                {
+                    throttle = value;
+                }
+            }
+        }
+
+        public byte Yaw
+        {
+            get
+            {
+                return yaw;
+            }
+
+            set
+            {
+                if (CheckValueRangeIncludeEdge(value, 0, 255))
+                {
+                    yaw = value;
+                }
+            }
+        }
+
+
+        public byte Pitch
+        {
+            get
+            {
+                return pitch;
+            }
+
+            set
+            {
+                if (CheckValueRangeIncludeEdge(value, 0, 255))
+                {
+                    pitch = value;
+                }
+            }
+        }
+
+        public byte Roll
+        {
+            get
+            {
+                return roll;
+            }
+
+            set
+            {
+                if (CheckValueRangeIncludeEdge(value, 0, 255))
+                {
+                    roll = value;
+                }
+            }
+        }
+    }
+}
