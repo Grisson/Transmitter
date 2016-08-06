4207cb9a92b79f34420b599649e10c228fd5118c
Jocelyn <jocelyn.sonsy@gmail.com>
2016-08-05 19:37:05 -0700

    first UI-4btn

--- Transmitter/Transmitter/Form1.cs
+++ Transmitter/Transmitter/Form1.cs
@@ -12,9 +12,73 @@ namespace Transmitter
 {
     public partial class Form1 : Form
     {
+        // a constructor needed or not?
+        private bool toggle1 = true;
+        private bool toggle2 = true;
+        private bool toggle3 = true;
+        private bool toggle4 = true;
+
         public Form1()
         {
             InitializeComponent();
+            SA.BackColor = Color.Green;
+            SB.BackColor = Color.Green;
+            SC.BackColor = Color.Green;
+            SD.BackColor = Color.Green;
         }
+
+        private void SA_Click(object sender, EventArgs e)
+        {
+            if (toggle1 == true)
+            {
+                SA.BackColor = Color.Red;
+            }
+            else
+            {
+                SA.BackColor = Color.Green;
+            }
+            toggle1 = !toggle1;
+        }
+
+        private void SB_Click(object sender, EventArgs e)
+        {
+            if (toggle2 == true)
+            {
+                SB.BackColor = Color.Red;
+            }
+            else
+            {
+                SB.BackColor = Color.Green;
+            }
+            toggle2 = !toggle2;
+        }
+
+        private void SC_Click(object sender, EventArgs e)
+        {
+            if (toggle3 == true)
+            {
+                SC.BackColor = Color.Red;
+            }
+            else
+            {
+                SC.BackColor = Color.Green;
+            }
+            toggle3 = !toggle3;
+        }
+
+        private void SD_Click(object sender, EventArgs e)
+        {
+            if (toggle4 == true)
+            {
+                SD.BackColor = Color.Red;
+            }
+            else
+            {
+                SD.BackColor = Color.Green;
+            }
+            toggle4 = !toggle4;
+        }
+
+
     }
 }
